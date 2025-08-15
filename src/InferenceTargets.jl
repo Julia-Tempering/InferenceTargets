dc1f3249a70b749e2f781334041d47283de18ff1
Alexandre Bouchard <bouchard@stat.ubc.ca>
2025-08-15 15:48:25 -0700

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.56

---
+++ src/InferenceTargets.jl
@@ -0,0 +1,5 @@
+module InferenceTargets
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using InferenceTargets
+using Test
+
+@testset "InferenceTargets.jl" begin
+    # Write your tests here.
+end
