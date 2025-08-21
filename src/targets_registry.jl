ebf8745a27a4c0593efc7124fb70054f7e56f9c2
Alexandre Bouchard <bouchard@stat.ubc.ca>
2025-08-20 17:25:22 -0700

    Faster test for doc deploy

--- src/targets_registry.jl
+++ src/targets_registry.jl
@@ -1,5 +1,5 @@
 const registry = Dict{Symbol, PackageSpec}(
-    :PosteriorDBTargets => PackageSpec(url = "https://github.com/Julia-Tempering/PosteriorDBTargets", rev = "f199984e0c3e754e9399fd3f4a1b220caa12f4f3"),
+    #:PosteriorDBTargets => PackageSpec(url = "https://github.com/Julia-Tempering/PosteriorDBTargets", rev = "f199984e0c3e754e9399fd3f4a1b220caa12f4f3"),
     # PRs welcome:  just add to this dictionary. 
     #               The key should be the module name (as a symbol). 
     #               Use a fixed commit or version, otherwise the PR will be rejected.
