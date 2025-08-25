81601ee81cb70ab5517e9fd9b25b9bc25174ee89
Alexandre Bouchard <bouchard@stat.ubc.ca>
2025-08-24 21:13:04 -0700

    Test OrbitPosteriorDB

--- src/targets_registry.jl
+++ src/targets_registry.jl
@@ -2,6 +2,7 @@ const registry = Dict{Symbol, PackageSpec}(
     :PosteriorDBTargets         => PackageSpec(url = "https://github.com/Julia-Tempering/PosteriorDBTargets", rev = "4823a1c9b55263ee9464dcda5357a51b308e62f8"),
     :TuringPigeonsExamples      => PackageSpec(url = "https://github.com/Julia-Tempering/TuringPigeonsExamples", rev = "0b09109682db72a1283c886d2855c9e8a4e4cce9"),
     :PigeonsExamples            => PackageSpec(url = "https://github.com/Julia-Tempering/PigeonsExamples", rev = "4ec988cf6568b4fb045bac328a76699f034aec10"),
+    :OrbitPosteriorDB           => PackageSpec(url = "https://github.com/Julia-Tempering/OrbitPosteriorDB.git", rev = "1f9c989bac7a817296fdd103abf28e7ba11ad275"), # TODO: move back to https://github.com/sefffal/OrbitPosteriorDB once tested and merged!
     #:BlangTargets               => PackageSpec(url = "https://github.com/Julia-Tempering/BlangTargets", rev = "85fb820aaa05ea511b6f1ef40f0b5edcea31c3b6"), # CI is stalling intermit'ly ?
     
     # PRs welcome:  just add to this dictionary. 
